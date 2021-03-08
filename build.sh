f010b8b17d799118c9e5c804fb0294d128c571df
David Haas <davidhaas6@gmail.com>
2021-03-07 23:59:36 -0600

    added multi options

--- build.sh
+++ build.sh
@@ -1,3 +1,15 @@
 # builds and runs the servers
-docker build -f local-dockerfile -t task_server:latest .
-docker-compose up
+echo $1
+if [[ $1 == "client" ]]; then
+echo "Building API environment"
+    docker build -f client-dockerfile -t api_client:latest .
+    docker-compose -f client-compose.yml up
+elif [[ $1 == "server" ]]; then
+    echo "Building Task Server environment"
+    docker build -f taskserver-dockerfile -t task_server:latest .
+    docker-compose -f client-compose.yml up
+else
+    echo "Building Local Dev Environment"
+    docker build -f local-dockerfile -t task_server:latest .
+    docker-compose -f client-compose.yml up
+fi
